8be508338932b95e509a7819188794256ce4b6de
AJ Bowen <aj@soulshake.net>
2023-12-17 18:24:16 +0100

    Try hardcoded chrome path

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 var prerender = require("./lib");
 const chrome_bin = process.env["GOOGLE_CHROME_BIN"];
 const chrome_shim = process.env["GOOGLE_CHROME_SHIM"];
+const chrome_path = "/app/.apt/usr/bin/google-chrome";
 console.log(`chrome_shim is ${chrome_shim}`);
 console.log(`chrome_bin is ${chrome_bin}`);
+console.log(`chrome_path is ${chrome_path}`);
 
 var server = prerender({
-  chromeLocation: chrome_bin,
+  chromeLocation: chrome_path,
   logRequests: true,
   captureConsoleLog: true,
   chromeFlags: [
