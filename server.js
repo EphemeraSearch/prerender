9b9ade3a4075241ac4c5d62129b4d3c4fe3898df
AJ Bowen <aj@soulshake.net>
2024-02-21 09:36:05 -0600

    refactor blockResources

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-var prerender = require("./lib");
+var prerender = require('./lib');
 
 var server = prerender({
   // chromeLocation: chrome_path,
   logRequests: true,
   captureConsoleLog: true,
   chromeFlags: [
-    "--no-sandbox",
-    "--headless",
-    "--disable-gpu",
-    "--remote-debugging-port=9222",
-    "--hide-scrollbars",
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--hide-scrollbars',
   ],
-  pageLoadTimeout: 40 * 1000,
+  pageLoadTimeout: Number(process.env['PAGE_LOAD_TIMEOUT']) || 40 * 1000,
 });
 
 server.use(prerender.sendPrerenderHeader());
@@ -22,8 +22,9 @@ server.use(prerender.forceCacheRefresh());
 server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-server.options.ttl = Number(process.env["CACHE_TTL"] || 60 * 60 * 24 * 3); // 3 days. Must come before `server.use(require("prerender-level-cache"))`
-console.log("server.options.ttl", server.options.ttl);
-server.use(require("prerender-level-cache"));
+server.options.ttl = Number(process.env['CACHE_TTL'] || 60 * 60 * 24 * 3); // 3 days. Must come before `server.use(require("prerender-level-cache"))`
+server.use(require('prerender-level-cache'));
+server.options.ttl = Number(process.env['CACHE_TTL'] || 60 * 60 * 24 * 3); // 3 days. Must come before `server.use(require("prerender-level-cache"))`
+console.log('server.options.ttl', server.options.ttl);
 
 server.start();
